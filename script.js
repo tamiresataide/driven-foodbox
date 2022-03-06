9fe89fb4707005b26e45623eb02b8fcfa8f3f889
tamiresataide <tamiresaataide@gmail.com>
2022-03-06 14:48:58 -0300

    adding basic javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+function escolherFrango(){
+    document.getElementById("frango").style.borderColor = "green";
+    document.getElementById("carne").style.borderColor = "white";
+}
+
+function escolherCarne(){
+    document.getElementById("carne").style.borderColor = "green";
+    document.getElementById("frango").style.borderColor = "white";
+}
+
+function escolherCoca(){
+    document.getElementById("coca").style.borderColor = "green";
+    document.getElementById("refresco").style.borderColor = "white";
+}
+
+function escolherRefresco(){
+    document.getElementById("refresco").style.borderColor = "green";
+    document.getElementById("coca").style.borderColor = "white";
+}
+
+function escolherPudim(){
+    document.getElementById("pudim").style.borderColor = "green";
+    document.getElementById("sorvete").style.borderColor = "white";
+}
+
+function escolherSorvete(){
+    document.getElementById("sorvete").style.borderColor = "green";
+    document.getElementById("pudim").style.borderColor = "white";
+}
